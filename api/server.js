71282a9ce5c22c2b2a38c62772c872c82d475b9d
brunacosta1705 <brunarochedo.bc@gmail.com>
2021-12-10 14:57:16 -0400

    implementando o login

--- api/server.js
+++ api/server.js
@@ -16,9 +16,10 @@ server.post('/cadastro', async(req, res) => {
     res.json(json_resposta)
 })
 
-server.post('/login', (req, res) => {
+server.post('/login', async(req, res) => {
     let usuario = req.body
-
+    let json_resposta = await select(usuario)
+    res.json(json_resposta)
 })
 
 server.listen(5987, () => {
@@ -46,6 +47,36 @@ async function insert(user) {
         return json = {
             "status": 0,
             "responseMessage": error
+        }
+    }
+}
+
+async function select(user) {
+    const db = require('../api/script/_database')
+
+    try {
+        await db.connect()
+
+        let usuario = user.login
+        let senha = user.senha
+
+        let valida = await db.query(`SELECT * FROM USUARIO WHERE EMAIL = '${usuario}' AND SENHA = '${senha}'`)
+
+        if (valida.rowCount == 1) {
+            return json = {
+                "status": 1,
+                "responseMessage": "logado"
+            }
+        } else {
+            return json = {
+                "status": 2,
+                "responseMessage": "usuario nao encontrado"
+            }
+        }
+    } catch (error) {
+        return json = {
+            "status": 0,
+            "responseMessage": erro
         }
     }
 }
--- front/public/script/main.js
+++ front/public/script/main.js
@@ -28,3 +28,31 @@ async function cadastro() {
         .catch(error => console.log('error', error));
 
 }
+
+async function login() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+        "login": document.getElementById("loginlogid").value,
+        "senha": document.getElementById("senhalogid").value
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:5987/login", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            if (result.status == 1) {
+                window.open('/front/view/home.html')
+            } else {
+                alert(result.responseMessage)
+            }
+        })
+        .catch(error => console.log('error', error))
+}
